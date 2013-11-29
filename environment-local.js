c4fc7556a42b60ebc2fb7e35c28ccc66075f466a
Taylor <trflagg@gmail.com>
2013-11-29 12:06:44 -0500

    Add test.js to demonstrate usage.
    test.js shows how to use the db and model objects along with
    an environment dict.

---
+++ environment-local.js
@@ -0,0 +1,4 @@
+module.exports.appName = "argie",
+module.exports.db = {
+    URL: 'mongodb://localhost:27017/local?auto_reconnect'
+}
---
+++ test.js
@@ -0,0 +1,35 @@
+var assert = require('assert'),
+	env = require('./environment-local'),
+	Db = require('./db');
+
+// Step 1 - Create Db object
+var db = new Db(env);
+
+// Step 2 - Register model by requiring it
+require('./model')(db);
+
+// Step 3 - Create new object
+var newModel = db.create('Model');
+
+// Step 4 - Modify object
+// ....
+var objId = newModel.id();
+
+// Step 5 - Save object
+// callback is optional
+db.save('Model', newModel, function(err) {
+	if (err !== null) {
+		console.log('error: ' + err);
+	}
+
+	// Step 6 - Look up object 
+	db.load('Model', {'_id': objId}, function(err, foundModel) {
+		if (err !== null) {
+			console.log('error: ' + err);
+		}
+		assert(foundModel.id().equals(objId));
+
+		// Step 7 - close db
+		db.close();
+	})
+});
